5b3e03eaf8466624c7684899cea819c12ed8ef6e
PavloShutz <pavlo050706@gmail.com>
2022-04-27 21:05:41 +0300

    Update human.py

--- human.py
+++ human.py
@@ -5,6 +5,8 @@ class Human:
         self.name = name
         self.surname = surname
         self.age = age
+        self.__full_info = [name, surname, age]
+        self.__index = 0
 
     def __str__(self):
         return f"{self.name.title()} {self.surname.title()}"
@@ -14,6 +16,17 @@ class Human:
 
     def __call__(self):
         return "Who called me?"
+
+    def __iter__(self):
+        self.__index = 0
+        return self
+
+    def __next__(self):
+        if self.__index >= len(self.__full_info):
+            raise StopIteration
+        index = self.__index
+        self.__index += 1
+        return self.__full_info[index]
 
     @property
     def fullname(self):
@@ -52,8 +65,7 @@ class Adult(Human):
     def elect(self):
         if self.election:
             return f"{self.name.title()}'s going to an election."
-        else:
-            return f"{self.name.title()} isn't going to an election."
+        return f"{self.name.title()} isn't going to an election."
 
 
 class Child(Human):
@@ -64,22 +76,34 @@ class Child(Human):
     def play(self):
         if self.play:
             return f"{self.name} is playing."
-        else:
-            return f"{self.name} doesn't want to play."
+        return f"{self.name} doesn't want to play."
 
 
 class Student(Human):
     study_hours = 300
-    marks = []
 
     def __init__(self, name, surname, age, subject):
         super().__init__(name, surname, age)
         self.subject = subject
+        self.marks = []
+        self.__index = 0
+        self.__full_info = [name, surname, age]
 
     def study(self):
         if Student.study_hours > 0:
             Student.study_hours -= 1.5
         return f"{self.name} studies {self.subject}"
+
+    def __iter__(self):
+        self.__index = 0
+        return self
+
+    def __next__(self):
+        if self.__index >= len(self.__full_info):
+            raise StopIteration
+        index = self.__index
+        self.__index += 1
+        return self.__full_info[index]
 
     @property
     def student_marks(self):
@@ -105,17 +129,12 @@ class Worker(Adult):
 class Teacher(Worker):
     increase_pay = 1.04
 
-    def __init__(self, name, surname, age, pay, subject, students=None, black_list_of_students=None):
+    def __init__(self, name, surname, age, pay, subject, *students):
         super().__init__(name, surname, age, pay)
         self.subject = subject
-        if students:
-            self.students = students
-        else:
-            self.students = []
-        if black_list_of_students:
-            self.black_list_of_students = black_list_of_students
-        else:
-            self.black_list_of_students = []
+        self.students = list(students) if students else []
+        self.black_list_of_students = []
+        self.__index = 0
 
     def __len__(self):
         return len(self.students)
@@ -126,10 +145,21 @@ class Teacher(Worker):
         except IndexError:
             return "No student was found by this index"
 
-    def evaluate_student(self, mark, student=Student):
+    def __iter__(self):
+        self.__index = 0
+        return self
+
+    def __next__(self):
+        if self.__index >= len(self.students):
+            raise StopIteration
+        index = self.__index
+        self.__index += 1
+        return self.students[index]
+
+    def evaluate_student(self, mark, student):
         if type(mark) is not int:
-            raise Exception(f"mark must be an integer, except got: {mark}")
-        if student.subject == self.subject and student in self.students:
+            raise TypeError(f"mark must be an integer, except got: {mark}")
+        if student.subject == self.subject and student in self.students and isinstance(student, Student):
             student.marks.append(mark)
 
     def accept_student(self, *students):
@@ -140,7 +170,7 @@ class Teacher(Worker):
     def remove_student(self, student):
         if student in self.students or Student.study_hours == 0:
             self.students.remove(student)
-        return f"{student} was removed."
+            return f"{student} was removed."
 
     def add_student_to_black_list(self, student):
         if student in self.students:
